54c09e9c54cc0ca9a10bf926e13f8b31c7846b71
Your Name <you@example.com>
2025-10-25 19:10:56 -0300

    se cambio la logica para tener una clase que maneje a otras clases,que tenga las cosas esenciales que deberian tener cada entidad u enemigo, la clase entidad.

---
+++ core/src/main/java/puppy/code/EntidadJuego.java
@@ -0,0 +1,57 @@
+package puppy.code;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+// Esta es nuestra Súperclase (o Clase Padre)
+// Es 'abstract' porque no queremos crear una 'EntidadJuego' genérica,
+// solo subclases específicas (Asteroide, Kamikaze, etc).
+public abstract class EntidadJuego {
+
+    // Usamos 'protected' para que las subclases (como EnemigoKamikaze)
+    // puedan acceder a ellas directamente
+    // Esto es parte de la Encapsulación
+    protected float x, y;
+    protected Sprite spr;
+    protected boolean destroyed = false;
+
+    // Constructor base
+    public EntidadJuego(Texture tx, float x, float y) {
+        this.x = x;
+        this.y = y;
+        this.spr = new Sprite(tx);
+        this.spr.setPosition(x, y);
+    }
+
+    // Método abstracto. Obliga a todas las subclases a implementar
+    // su propia lógica de 'update'
+    public abstract void update(float delta);
+
+    // Métodos comunes que todas las entidades tendrán
+    public void draw(SpriteBatch batch) {
+        spr.draw(batch);
+    }
+
+    public Rectangle getArea() {
+        return spr.getBoundingRectangle();
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    public void destruir() {
+        this.destroyed = true;
+    }
+
+    // Getters para la posición (útiles para la IA)
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+}
