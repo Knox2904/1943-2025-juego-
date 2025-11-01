baa7f875fb84c8adac183f8971fdf2540bdad615
Your Name <you@example.com>
2025-10-31 23:44:29 -0300

    arreglado el texto.

--- core/src/main/java/puppy/code/PantallaMenu.java
+++ core/src/main/java/puppy/code/PantallaMenu.java
@@ -27,7 +27,7 @@ public class PantallaMenu implements Screen {
 		game.getBatch().setProjectionMatrix(camera.combined);
 
 		game.getBatch().begin();
-		game.getFont().draw(game.getBatch(), "Bienvenido a Space Navigation !", 140, 400);
+		game.getFont().draw(game.getBatch(), "Bienvenido a 2025 !", 140, 400);
 		game.getFont().draw(game.getBatch(), "Pincha en cualquier lado o presiona cualquier tecla para comenzar ...", 100, 300);
 
 		game.getBatch().end();
